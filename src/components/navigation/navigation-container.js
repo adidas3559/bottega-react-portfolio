38768296306e51e3668ca1fbab65d7ebaa156467
adidas3559 <forbes3559@gmail.com>
2020-07-25 15:01:04 -0500

    new commit after add

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,37 +1,76 @@
-import React, { Component } from "react";
+import React from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { withRouter } from "react-router";
 import { NavLink } from 'react-router-dom';
 
-export default class NavigatoinContainer extends Component {
+const NavigationComponent = (props) => {
 
-    constructor() {
-        super();
-
-
-    }
-
-
-    render() {
+    const dynamicLink = (route, linkText) => {
         return (
-            <div>
-                
-                <NavLink exact to="/" activeClassName="nav-link-active">
-                    Home
+            <div className="nav-link-wrapper">
+                <NavLink to={route} activeClassName="nav-link-active">
+                    {linkText}
                 </NavLink>
-
-                <NavLink to="/about-me" activeClassName="nav-link-active">
-                    About Me
-                </NavLink>
-
-                <NavLink to="/contact" activeClassName="nav-link-active">
-                    Contact
-                </NavLink>
-
-                <NavLink to="/blog" activeClassName="nav-link-active">
-                    Blog
-                </NavLink>
-
-                {false ? <button>Add Blog</button> : null}
             </div>
         );
-    }
-}
+    };
+
+
+    const handleSignOut = () => {
+        axios.delete("https://api.devcamp.space/logout", {withCredentials: true}).then(response => {
+            if(response.status === 200) {
+                props.history.push("/");
+                props.handleSuccesfulLogout();
+            }
+            return response.data;
+        }).catch(error => {
+            console.log("Error signing out", error);
+        });
+    };
+
+    return (
+        <div className="nav-wrapper">
+            <div className="left-side">
+                <div className="nav-link-wrapper">
+                    <NavLink exact to="/" activeClassName="nav-link-active">
+                        Home
+                    </NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink to="/about-me" activeClassName="nav-link-active">
+                        About Me
+                    </NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink to="/contact" activeClassName="nav-link-active">
+                        Contact
+                    </NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink to="/blog" activeClassName="nav-link-active">
+                        Blog
+                    </NavLink>
+                </div>
+
+                {props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/portfolio-manager", "Portfolio Manager") : null}
+
+            </div>
+            
+            <div className="right-side">
+                STEPHEN FORBES
+                {props.loggedInStatus === "LOGGED_IN" ? 
+                <a onClick={handleSignOut}>
+                    <FontAwesomeIcon icon="sign-out-alt" />
+                </a>
+                : null}
+            </div>
+            
+        </div>
+    );
+};
+
+export default withRouter(NavigationComponent);
--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,24 @@
 import React from "react";
+import profilePic from "../../../static/assets/images/me.jpg"
 
 export default function() {
     return (
-        <div>
-            About Me
+        <div className="content-page-wrapper">
+            <div className="left-column"
+                style={{
+                    background: "url(" + profilePic + ") no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center"
+                }}
+            >
+
+            </div>
+
+            <div className="right-column">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra leo vel neque eleifend sodales nec eu erat. Nullam dapibus mollis diam, nec tristique justo gravida ac. Nullam cursus sem malesuada mi congue molestie. Vivamus tempus eros nec arcu aliquet rutrum. Morbi pharetra est nibh, eu scelerisque leo placerat ut. Morbi molestie urna vel velit hendrerit mattis. Fusce et eros quis purus rhoncus auctor non nec velit. Vivamus nec risus est. Sed nisi lorem, elementum nec laoreet ut, sagittis ac risus. Ut finibus velit ac rhoncus rutrum. Nullam et ultricies nibh, sed imperdiet sapien. Nulla a interdum arcu, et scelerisque lorem. Donec at vestibulum lectus. Sed euismod condimentum feugiat. Proin accumsan euismod risus eu dapibus. Quisque eu leo a justo interdum vestibulum sed eget ex.
+
+                Vestibulum dignissim arcu non urna aliquam, sagittis bibendum dui sollicitudin. Ut sed dui eget magna accumsan dictum ut vel augue. Nullam molestie diam ornare elit porttitor, ut varius purus imperdiet. Aliquam quis efficitur ipsum. Nulla venenatis felis finibus commodo fermentum. In rutrum ex orci, at condimentum quam laoreet vel. Vivamus a tellus bibendum, malesuada sapien quis, ultrices quam. Curabitur quis finibus ante. Quisque mattis, arcu ac tincidunt imperdiet, mauris purus ullamcorper purus, eu efficitur ex neque vel ex. Nullam in leo aliquam, suscipit eros eu, sodales justo. Suspendisse sodales ex in quam hendrerit, et laoreet ipsum sagittis. Pellentesque suscipit accumsan nisl scelerisque lacinia.
+            </div>
         </div>
     );
 }
